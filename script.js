617237953fbb1afcee003e84abd2606aa2df9bad
Sreenath k k <sreenathkk00@gmail.com>
2025-01-02 21:34:43 +0530

    Update "What I Do" Section & Add Themed Courses Carousel

--- script.js
+++ script.js
@@ -256,3 +256,24 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// Owl Carousel
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  autoplay: true, 
+  autoplayTimeout: 3000, 
+  autoplayHoverPause: true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:2
+      }
+  }
+})
+
